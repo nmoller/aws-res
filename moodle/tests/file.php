335cd8f271fa80f8ce8d5a2d3f287643c046a743
Nelson Moller <nmoller.c@gmail.com>
2020-07-28 08:00:11 -0400

    Modifier script pour plusieurs fichiers dans la même date.

--- moodle/tests/file.php
+++ moodle/tests/file.php
@@ -5,8 +5,9 @@ require(__DIR__.'/../../../config.php');
 
 Avec un cli du type, on télécharge le fichier d'inscriptions:
 
-wget http://moo-p-publi-172qihvfxhj8t-1730582042.ca-central-1.elb.amazonaws.com/local/uqregistration/download/file.php?date=20200608 -O test.txt
-*/
+curl https://dmwrmdhzg4g1f.cloudfront.net/local/uqregistration/download/file.php?date=20200728 \
+-o /opt/oracle/var/extras/test01.txt
+ */
 
 $date = required_param('date', PARAM_INT);
 
@@ -16,17 +17,17 @@ $new = get_config('local_uqregistration', 'requetes_rep_new');
 $download_folder = $requetes_rep_location . $new . $date ;
 
 $files = glob( "$download_folder/*.txt");
+$nbrFiles = count($files);
 
-if (isset($files[0]) and file_exists($files[0])) {
-	header('Content-Type: text/plain');
-	header("Content-Disposition: attachment; filename=\"" . basename($files[0]) . "\";");
-	header('Content-Length: ' . filesize($files[0]));
-	ob_clean();
-	flush();
-	readfile($files[0]); 
-	exit;
+if ($nbrFiles > 0 and isset($files[$nbrFiles-1]) and file_exists($files[$nbrFiles-1])) {
+    header('Content-Type: text/plain');
+    header("Content-Disposition: attachment; filename=\"" . basename($files[$nbrFiles-1]) . "\";");
+    header('Content-Length: ' . filesize($files[$nbrFiles-1]));
+    ob_clean();
+    flush();
+    readfile($files[$nbrFiles-1]);
+    exit;
 }
-
 
 
 
